4828f25e0be3f3c138acec8f0923058556a4ae44
Jordan Coblin <jcoblin@jcoblin-mac.tsi.lan>
2018-12-10 20:58:34 -0800

    pixel rendering working

--- static/js/mandelbrot.js
+++ static/js/mandelbrot.js
@@ -1,11 +1,29 @@
 function draw() {
-	var canvas = document.getElementById('myCanvas');
+	const canvas = document.getElementById('myCanvas');
 	if (!canvas.getContext) {
 	  return;
 	}
 
-	var ctx = canvas.getContext('2d');
+	const ctx = canvas.getContext('2d');
 	ctx.fillStyle = "#FF0000";
 	ctx.fillRect(0,0,150,150);
 	console.log("this code runs!");
+
+	const imageData = ctx.createImageData(60, 60);
+
+	// Iterate through every pixel
+	for (let i = 0; i < imageData.data.length; i += 4) {
+		// Modify pixel data
+		imageData.data[i + 0] = i%255;  // R value
+		imageData.data[i + 1] = i%255;    // G value
+		imageData.data[i + 2] = i%255;  // B value
+		imageData.data[i + 3] = i%255;  // A value
+	}
+
+	// Draw image data to the canvas
+	ctx.putImageData(imageData, 50, 50);
+}
+
+function mandelbrot() {
+	return;
 }
